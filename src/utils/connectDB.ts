a6d0c4289c7ae8b65e3c8c534dfd828fd24b632f
paulonova <paulonova144@gmail.com>
2023-07-18 23:43:26 -0300

    new uppdate

--- src/utils/connectDB.ts
+++ src/utils/connectDB.ts
@@ -1,36 +1,10 @@
-import mongoose from 'mongoose';
+import { MongoClient } from "mongodb";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error('Please add the database url in .env.local file');
-}
+const DATABASE_URL = process.env.DATABASE_URL;
+const options = {}
 
-const DATABASE_URL: string = process.env.DATABASE_URL;
+if(!DATABASE_URL) throw new Error("Please add you mongo db url to env.local file");
 
-let globalWithMongoose = global as typeof globalThis & {
-  mongoose: any;
-};
+let client = new MongoClient(DATABASE_URL, options)
+let clentPromise
 
-let cached = globalWithMongoose.mongoose;
-if (!cached) {
-  cached.globalWithMongoose.mongoose = { conn: null, promise: null };
-}
-
-async function connectDB() {
-  if (cached.conn) {
-    return cached.conn;
-  }
-
-  if (!cached.promise) {
-    const options = {
-      bufferCommands: false,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    };
-    cached.promise = mongoose
-      .connect(DATABASE_URL, options)
-      .then((mongoose) => {
-        console.log('Mongoose: ', mongoose);
-        return mongoose;
-      });
-  }
-}
